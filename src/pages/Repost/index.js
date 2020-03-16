3c93db4ecc220b30a7412412f5f36151b608235b
Lucas Cassilha <lucaszawadneak@gmail.com>
2020-03-16 16:51:08 -0300

    Repost Style Screen

--- src/pages/Repost/index.js
+++ src/pages/Repost/index.js
@@ -1,8 +1,49 @@
 import React from 'react';
-import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
-import { Container } from './styles';
+import Share from 'react-native-share';
 
-export default function Repost() {
-  return <Container />;
+import {
+  Container,
+  Box,
+  Picture,
+  Description,
+  RepostButton,
+  Gradient,
+  Label,
+} from './styles';
+
+export default function Repost({ route }) {
+  const { posts } = route.params;
+
+  const handleShare = () => {
+    const shareOptions = {
+      message: 'some message',
+      url: posts.image,
+      social: Share.Social.INSTAGRAM_STORIES,
+      stickerImage: `data:link,${posts.image}`,
+    };
+    Share.open(shareOptions);
+  };
+
+  const repostDescription = `#REPOST @${posts.author} \n\n${posts.description}`;
+
+  return (
+    <Container>
+      <Box>
+        <Picture
+          source={{
+            uri: posts.image,
+          }}
+        />
+        <Description>{repostDescription}</Description>
+        <RepostButton onPress={handleShare}>
+          <Gradient>
+            <Icon name="instagram" color="#fff" size={25} />
+            <Label>Repost</Label>
+          </Gradient>
+        </RepostButton>
+      </Box>
+    </Container>
+  );
 }
