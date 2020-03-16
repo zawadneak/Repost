b828386c77573795864008a808544514501fb5bb
Lucas Cassilha <lucaszawadneak@gmail.com>
2020-03-16 10:00:15 -0300

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import 'react-native-gesture-handler';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default function App() {
+  return <View />;
+}
